c8d016a9e9f3dda782d8df936b1f512e7ddf2579
Jameson Gagnepain <Jameson.Gagnepain@Hollister.com>
2019-04-08 15:34:44 -0500

    Add console test project

---
+++ BrickABracket.Console/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using BrickABracket.Core;
+using BrickABracket.Core.Services;
+using BrickABracket.Models;
+using BrickABracket.NXT;
+using BrickABracket.Derby;
+using BrickABracket.RoundRobin;
+using BrickABracket.SingleElimination;
+using BrickABracket.SwissSystem;
+
+namespace BrickABracket.Console
+{
+    class Program
+    {
+        private static IServiceProvider _serviceProvider;
+        private static IContainer _container;
+        static void Main(string[] args)
+        {
+            RegisterServices();
+            
+            using(var scope = _container.BeginLifetimeScope())
+            {
+                var resolver = scope.Resolve<Func<string, Nxt>>();
+                var nxt = resolver("COM3");
+                System.Console.WriteLine(nxt.Connect());
+            }
+
+            System.Console.WriteLine("Hello World!");
+            DisposeServices();
+        }
+
+        private static void RegisterServices()
+        {
+            var serviceCollection = new ServiceCollection();
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new ModelsModule());
+            builder.RegisterModule(new CoreModule());
+            builder.RegisterModule(new NxtModule());
+            builder.RegisterModule(new DerbyModule());
+            builder.RegisterModule(new RoundRobinModule());
+            builder.RegisterModule(new SingleEliminationModule());
+            builder.RegisterModule(new SwissSystemModule());
+
+            builder.Populate(serviceCollection);
+            _container = builder.Build();
+            _serviceProvider = new AutofacServiceProvider(_container);
+        }
+
+        private static void DisposeServices()
+        {
+            if(_serviceProvider == null)
+            {
+                return;
+            }
+            if (_serviceProvider is IDisposable)
+            {
+                ((IDisposable)_serviceProvider).Dispose();
+            }
+        }
+    }
+}
