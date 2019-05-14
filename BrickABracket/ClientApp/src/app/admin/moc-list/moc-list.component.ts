89ce0694248de8c9d220c0651b575b24d43615c7
Jameson Gagnepain <jameson42@gmail.com>
2019-05-13 21:41:30 -0500

    start moc list

--- BrickABracket/ClientApp/src/app/admin/moc-list/moc-list.component.ts
+++ BrickABracket/ClientApp/src/app/admin/moc-list/moc-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MocService } from 'src/app/core/mocs/moc.service';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Moc } from 'src/app/core/mocs/moc';
+import { TournamentService } from 'src/app/core/tournaments/tournament.service';
 
 @Component({
   selector: 'app-moc-list',
@@ -8,10 +13,29 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MocListComponent implements OnInit {
 
   @Input() tournamentId: number;
+  private mocs$: Observable<Array<Moc>>;
 
-  constructor() { }
+  constructor(
+    private mocs: MocService,
+    private tournaments: TournamentService,
+    ) { }
 
   ngOnInit() {
+    if (this.tournamentId) {
+      this.mocs$ = combineLatest(
+        this.mocs.mocs, 
+        this.tournaments.tournament
+        ).pipe(
+          map(([ma, t]) => ma.filter(m => t.mocIds.indexOf(m._id) >= 0))
+        );
+    } else {
+      this.mocs$ = this.mocs.mocs;
+    }
+    // TODO: Get competitor name!
+  }
+
+  addMoc() {
+    // TODO: Go to new moc page based on current router url
   }
 
 }
