2ac2201da13b98e8eac4edb6dff030052973bbed
Jameson Gagnepain <jameson42@gmail.com>
2019-05-16 21:34:27 -0500

    Fix home subscriptions

--- BrickABracket/ClientApp/src/app/admin/home/home.component.ts
+++ BrickABracket/ClientApp/src/app/admin/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { TournamentService } from 'src/app/core/tournaments/tournament.service';
-import { CompetitorService } from 'src/app/core/competitors/competitor.service';
-import { MocService } from 'src/app/core/mocs/moc.service';
-import { ClassificationService } from 'src/app/core/classifications/classification.service';
-import { DeviceService } from 'src/app/core/devices/device.service';
+import { Observable } from 'rxjs';
+
+import { ClassificationService, CompetitorService, 
+  DeviceService, TournamentService } from '@bab/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -11,36 +11,31 @@ import { DeviceService } from 'src/app/core/devices/device.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private tournamentCount:number=0;
-  private competitorCount:number=0;
-  private mocCount:number=0;
-  private classificationCount:number=0;
-  private deviceCount:number=0;
+  private tournamentCount$: Observable<number>;
+  private competitorCount$: Observable<number>;
+  private classificationCount$: Observable<number>;
+  private deviceCount$: Observable<number>;
 
   constructor(
     private tournaments: TournamentService,
     private competitors: CompetitorService,
-    private mocs: MocService,
     private classifications: ClassificationService,
     private devices: DeviceService
   ) { }
 
   ngOnInit() {
-    this.tournaments.summaries.subscribe(t=>{
-      this.tournamentCount = t.length;
-    });
-    this.competitors.competitors.subscribe(c => {
-      this.competitorCount = c.length;
-    });
-    this.mocs.mocs.subscribe(m => {
-      this.mocCount = m.length;
-    });
-    this.classifications.classifications.subscribe(c => {
-      this.classificationCount = c.length;
-    })
-    this.devices.devices.subscribe(d => {
-      this.deviceCount = d.length;
-    })
+    this.tournamentCount$ = this.tournaments.summaries.pipe(
+      map(t => t.length)
+    );
+    this.competitorCount$ = this.competitors.competitors.pipe(
+      map(c => c.length)
+    );
+    this.classificationCount$ = this.classifications.classifications.pipe(
+      map(c => c.length)
+    );
+    this.deviceCount$ = this.devices.devices.pipe(
+      map(d => d.length)
+    );
   }
 
 }
